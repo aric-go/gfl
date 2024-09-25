3534473f54c71c3782cc2665a8e98804e628a58f
aric <1290657123@qq.com>
2024-09-25 23:45:08 +0800

    feat: new impl push orgin head

--- cmd/publish.go
+++ cmd/publish.go
@@ -11,21 +11,11 @@ var publishCmd = &cobra.Command{
 	Use:   "publish",
 	Short: "发布当前分支",
 	Run: func(cmd *cobra.Command, args []string) {
-		// 获取当前分支名称
-		currentBranch, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
-		if err != nil {
-			fmt.Println("无法获取当前分支:", err)
-			return
-		}
-		branchName := string(currentBranch)
-		branchName = branchName[:len(branchName)-1] // 移除末尾换行符
-
-		// 执行命令: git push origin -u feature/aric/new-feature
-		_, err = exec.Command("git", "push", "origin", "-u", branchName).Output()
-		if err != nil {
-			fmt.Println("推送失败:", err)
+		// 执行命令: git push -u origin HEAD
+		if err := exec.Command("git", "push", "-u", "origin", "HEAD").Run(); err != nil {
+			fmt.Println("推送当前分支失败:", err)
 		} else {
-			fmt.Printf("已推送当前分支: %s 到远程仓库\n", branchName)
+			fmt.Println("已推送当前分支到远程仓库，并设置上游分支")
 		}
 	},
 }
