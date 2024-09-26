841417a0215d2abb3c78c04a2ba72f313726682e
aric <1290657123@qq.com>
2024-09-26 21:55:13 +0800

    chore: rm comments

--- utils/shell.go
+++ utils/shell.go
@@ -1,10 +1,3 @@
-/**
- * @Author: aric 1290657123@qq.com
- * @Date: 2024-09-26 14:08:24
- * @LastEditors: aric 1290657123@qq.com
- * @LastEditTime: 2024-09-26 14:14:19
- * @FilePath: utils/shell.go
- */
 package utils
 
 import "os/exec"
