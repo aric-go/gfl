5b09aacbc29c3bf4d7f329800e6cb50db905d146
aric <1290657123@qq.com>
2024-10-11 21:41:41 +0800

    fix: upt bump version

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -4,9 +4,9 @@
 version=$(cat VERSION)
 
 # 使用 awk 增加小版本号
-new_version=$(echo $version | awk -F. -v OFS=. '{$NF++;print}')
+new_version=$(echo "$version" | awk -F. -v OFS=. '{$NF++;print}')
 
 # 更新版本文件
-echo $new_version > VERSION
+echo "$new_version" > VERSION
 
 echo "版本已更新为: $new_version"
