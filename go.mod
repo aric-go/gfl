584e4b98fe5be7a628f688f35ab1ae4c3d2dfa97
aric <1290657123@qq.com>
2024-10-26 22:41:37 +0800

    feat: add semver - increase

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github-flow
 
-go 1.21.0
+go 1.22.0
+
+toolchain go1.23.0
 
 require (
 	github.com/spf13/cobra v1.8.1
@@ -17,6 +19,7 @@ require (
 	github.com/mattn/go-isatty v0.0.8 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/term v0.1.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
---
+++ utils/semver.go
@@ -0,0 +1,56 @@
+package utils
+
+import (
+	"fmt"
+	"golang.org/x/mod/semver"
+	"strconv"
+	"strings"
+)
+
+// IncrementVersion 根据传入的 part (MAJOR, MINOR, PATCH) 更新相应版本号
+func IncrementVersion(currentVersion, part string) (string, error) {
+	// 检查版本号是否有效
+	if !semver.IsValid(currentVersion) {
+		return "", fmt.Errorf("无效的版本号: %s", currentVersion)
+	}
+
+	// 去掉前缀 "v"
+	version := currentVersion[1:]
+	parts := strings.Split(version, ".")
+	if len(parts) != 3 {
+		return "", fmt.Errorf("版本号格式错误: %s", currentVersion)
+	}
+
+	// 解析 MAJOR, MINOR, PATCH 为整数
+	major, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return "", fmt.Errorf("转换 MAJOR 部分出错: %v", err)
+	}
+	minor, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return "", fmt.Errorf("转换 MINOR 部分出错: %v", err)
+	}
+	patch, err := strconv.Atoi(parts[2])
+	if err != nil {
+		return "", fmt.Errorf("转换 PATCH 部分出错: %v", err)
+	}
+
+	// 根据传入的 part 值递增对应部分
+	switch strings.ToUpper(part) {
+	case "MAJOR":
+		major++
+		minor = 0
+		patch = 0
+	case "MINOR":
+		minor++
+		patch = 0
+	case "PATCH":
+		patch++
+	default:
+		return "", fmt.Errorf("无效的版本部分: %s", part)
+	}
+
+	// 构造新的版本号
+	newVersion := fmt.Sprintf("v%d.%d.%d", major, minor, patch)
+	return newVersion, nil
+}
