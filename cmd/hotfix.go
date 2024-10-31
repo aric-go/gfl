3e9321dd4c5c7257427a98de539a231314f7526d
aric <1290657123@qq.com>
2024-10-31 18:25:08 +0800

    feat: hotfix -b when manual

--- cmd/hotfix.go
+++ cmd/hotfix.go
@@ -10,6 +10,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var branchFlag string
+
 // hotfixCmd represents the hotfix command
 var hotfixCmd = &cobra.Command{
 	Use:   "hotfix",
@@ -18,17 +20,23 @@ var hotfixCmd = &cobra.Command{
 		config := utils.ReadConfig()
 		featureName := args[0] // 从参数中获取Hotfix名称
 		branchName := fmt.Sprintf("hotfix/%s/%s", config.Nickname, featureName)
-		baseRemoteBranch := fmt.Sprintf("origin/%s", config.HotfixBaseBranch)
 
-		// 执行命令: git fetch origin develop
+		// 如果用户传入了 -b 则使用用户的分支，否则使用配置的默认分支
+		baseBranch := config.HotfixBaseBranch
+		if branchFlag != "" {
+			baseBranch = branchFlag
+		}
+		baseRemoteBranch := fmt.Sprintf("origin/%s", baseBranch)
+
+		// 执行命令: git fetch origin
 		if !skipFetch {
-			command1 := fmt.Sprintf("git fetch origin")
+			command1 := "git fetch origin"
 			if err := utils.RunCommandWithSpin(command1, " 正在同步远程分支...\n"); err != nil {
 				return
 			}
 		}
 
-		// 执行命令: git checkout -b feature/aric/new-feature origin/develop
+		// 执行命令: git checkout -b hotfix/aric/new-feature origin/develop
 		command2 := fmt.Sprintf("git checkout -b %s %s", branchName, baseRemoteBranch)
 		if err := utils.RunCommandWithSpin(command2, " 正在创建Hotfix分支...\n"); err != nil {
 			return
@@ -38,5 +46,7 @@ var hotfixCmd = &cobra.Command{
 }
 
 func init() {
+	hotfixCmd.Flags().StringVarP(&branchFlag, "branch", "b", "", "指定一个自定义的 base 分支")
+	hotfixCmd.Flags().BoolVarP(&skipFetch, "skip-fetch", "s", false, "跳过 git fetch 步骤")
 	rootCmd.AddCommand(hotfixCmd)
 }
