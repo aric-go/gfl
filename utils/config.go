ee11e877cc1cb403341b744fbceb4e991f988215
aric <1290657123@qq.com>
2024-12-26 16:19:52 +0800

    feat: add publish list

--- utils/config.go
+++ utils/config.go
@@ -1,32 +1,39 @@
 package utils
 
 import (
-	"fmt"
-	"os"
+  "fmt"
+  "os"
 
-	"gopkg.in/yaml.v3"
+  "gopkg.in/yaml.v3"
 )
 
+type PublishItem struct {
+  Name   string `yaml:"name"`
+  Source string `yaml:"source"`
+  Target string `yaml:"target"`
+}
+
 type YamlConfig struct {
-	Debug            bool   `yaml:"debug"`
-	DevBaseBranch    string `yaml:"devBaseBranch"`
-	ProductionBranch string `yaml:"productionBranch"`
-	Nickname         string `yaml:"nickname"`
-	GitlabHost       string `yaml:"gitlabHost"`
+  Debug            bool          `yaml:"debug"`
+  DevBaseBranch    string        `yaml:"devBaseBranch"`
+  ProductionBranch string        `yaml:"productionBranch"`
+  Nickname         string        `yaml:"nickname"`
+  GitlabHost       string        `yaml:"gitlabHost"`
+  PublishList      []PublishItem `yaml:"publishList"`
 }
 
 func ReadConfig() *YamlConfig {
-	data, err := os.ReadFile(".gflow.config.yml")
-	if err != nil {
-		fmt.Println("读取配置文件失败:", err)
-		return nil
-	}
+  data, err := os.ReadFile(".gflow.config.yml")
+  if err != nil {
+    fmt.Println("读取配置文件失败:", err)
+    return nil
+  }
 
-	var config YamlConfig
-	if err := yaml.Unmarshal(data, &config); err != nil {
-		fmt.Println("解析配置文件失败:", err)
-		return nil
-	}
+  var config YamlConfig
+  if err := yaml.Unmarshal(data, &config); err != nil {
+    fmt.Println("解析配置文件失败:", err)
+    return nil
+  }
 
-	return &config
+  return &config
 }
